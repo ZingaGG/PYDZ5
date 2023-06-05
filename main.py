ee79cf32c5825eb7827bc504322b4864ac0f23cc
ZingaG <trad_bar@mail.ru>
2023-06-05 20:04:15 +0300

    Task 2 completed

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 # Task 1
 
-def AinB(a, b):
-    if b == 1:
+# def AinB(a, b):
+#     if b == 1:
+#         return a
+#     else:
+#         a = a * AinB(a, b-1)
+#         return a
+    
+# a, b = 2, 3
+# print(AinB(a,b))
+
+# Task 2
+
+def sum(a, b):
+    if b == 0:
         return a
     else:
-        a = a * AinB(a, b-1)
-        return a
+        return sum(a+1, b-1)
+        
     
-a, b = 2, 3
-print(AinB(a,b))
+a, b = 2, 5
+print(f"Result = {sum(a,b)}")
